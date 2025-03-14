a7fe2fdab44e3b8e30060f89ccc9e4867bac6b43
Amjad-Tp <amjadtpvalakkai@gmail.com>
2025-03-14 16:03:41 +0530

    edited user details screen

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -4,7 +4,6 @@ import 'package:foodpe/functions/db_functions.dart';
 import 'package:foodpe/model/user_model.dart';
 import 'package:foodpe/screens/user_edit_screen.dart';
 
-
 class UserScreen extends StatelessWidget {
   final User user;
   const UserScreen({super.key, required this.user});
@@ -13,98 +12,91 @@ class UserScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ValueListenableBuilder<User>(
-          valueListenable: userNotifier,
-          builder: (context, user, child) {
-            return ListView(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 15, top: 20),
-                  child: Text(
-                    'User',
-                    style: TextStyle(fontSize: 34, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.grey[300],
-                    image: user.imagePath != null
-                        ? DecorationImage(
-                            image: FileImage(
-                              File(user.imagePath!),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 15, top: 20),
+              child: Text(
+                'Details',
+                style: TextStyle(fontSize: 34, fontWeight: FontWeight.w500),
+              ),
+            ),
+            const SizedBox(height: 100),
+            Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: 300,
+                height: 450,
+                child: ValueListenableBuilder<User>(
+                  valueListenable: userNotifier,
+                  builder: (context, user, child) {
+                    return Card(
+                      elevation: 20,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: IconButton.outlined(
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (ctx) => UserEditScreen(
+                                            user: user,
+                                          )));
+                                },
+                                icon: const Icon(Icons.edit_rounded),
+                              ),
                             ),
-                          )
-                        : null,
-                  ),
-                  child: user.imagePath == null
-                      ? const Icon(
-                          Icons.person,
-                          size: 80,
-                          color: Colors.grey,
-                        )
-                      : null,
-                ),
-                const SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    user.name,
-                    style: const TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    user.email,
-                    style: const TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    user.phoneNumber,
-                    style: const TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 130),
-                Align(
-                  alignment: Alignment.center,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: const Color(0xFFD9D9D9),
-                      padding: const EdgeInsets.symmetric(horizontal: 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
+                            const SizedBox(height: 15),
+                            Container(
+                              height: 250,
+                              width: 250,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.grey[300],
+                                image: user.imagePath != null
+                                    ? DecorationImage(
+                                        image: FileImage(
+                                          File(user.imagePath!),
+                                        ),
+                                      )
+                                    : null,
+                              ),
+                              child: user.imagePath == null
+                                  ? const Icon(
+                                      Icons.person,
+                                      size: 80,
+                                      color: Colors.grey,
+                                    )
+                                  : null,
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              user.name,
+                              style: const TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              user.phoneNumber,
+                              style: const TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => UserEditScreen(user: user,)));
-                    }, 
-                    child: const Text(
-                      "Edit User",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
+                    );
+                  },
                 ),
-              ],
-            );
-          },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-  
 }
